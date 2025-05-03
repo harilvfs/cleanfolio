85924ddc2521633b5bb467116056c3cfc743d477
Hari Chalise <ingoprivate@gmail.com>
2025-05-03 22:34:37 +0545

    migrate to bun

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,15 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
+    Source →{' '}
     <a
       href='https://github.com/rjshkhr/cleanfolio'
       className='link footer__link'
     >
-      Source From Raj Shekhar
+      Raj Shekhar
     </a>
   </footer>
 )
 
 export default Footer
+
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://harilvfs.github.io/cleanfolio',
+  homepage: '.',
   title: 'PXLR.',
 }
 
